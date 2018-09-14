91b02ec7f4c230341bb1cf1fea82073011bc64ea
Blues <i@iblues.name>
2018-09-14 17:17:35 +0800

    测试扩展包 基本完成

---
+++ src/Model/Test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lanyang
+ * Date: 2018/9/13
+ * Time: 下午4:17
+ */
+
+namespace Iblues\Test\Model;
+
+
+class Test
+{
+    function test(){
+        return true;
+    }
+
+}
---
+++ tests/testTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Iblues\Test\Tests;
+
+use Iblues\Test\Model\Test;
+use PHPUnit\Framework\TestCase;
+
+class testTest extends TestCase
+{
+
+    public function testTest(){
+        $data = new Test();
+        $return = $data->test();
+        $this->assertEquals($return,1);
+    }
+}
